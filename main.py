d705496f41ca2f98415213e177c8d81d2f150426
EC2 Default User <ec2-user@ip-172-16-0-65.ec2.internal>
2024-04-07 23:31:22 +0000

    worked on the loop, ficed the timer

--- main.py
+++ main.py
@@ -1,30 +1,52 @@
-#Looking tomake a fighting game 
-#an AI fighting game RPG where the characters will evolve in different ways
-#first will be a basic test game 
-#then plan to add graphical elements
-#
-#CONSTANTS:
-# events per cycle
-# directions 
-# moves
-#VARIABLES:
-# character position 
-# health 
-# timer
-#CYCLE
-#Start -> 
-#   Status -> 
-#       time: arena.time
-#       health: p1.health, p2.health
-#       position: p1.position, p2.position
-#       last action p1.last action, p2.last action
-#   Player 1 decide -> 
-#       Move.nextPosition
-#       Attack.nextAttack
-#       Defend
-#   Player 2 decide - > 
-#       Move
-#       Attack
-#       Defend
-#   Execute ->
-# END -> 
+MAX_CYCLE = 60
+MAX_HEALTH = 16
+MAX_ATTACK = 12
+
+
+p1_alive = True 
+p2_alive = True
+p1_health = MAX_HEALTH
+p2_health = MAX_HEALTH
+timer = MAX_CYCLE
+
+p1Vp2 = 0 
+p2Vp1 = 0 
+stillTime = 1 
+
+
+PLAY = True 
+cycle = 0
+#Main Cylce
+while PLAY:
+    #Status
+    print("---")
+    print(f'Start Cycle Continue: {PLAY}')
+    print(f'Time: {timer}')
+    print(f'p1Vp2: {p1Vp2}')
+    print(f'p2Vp1: {p2Vp1}')
+    print(f'p1_health: {p1_health}')
+    print(f'p2_health: {p2_health}')
+    
+    #PLAYER
+    #Player 1 decide
+    p1_attack = 1
+    p1_defend = 1
+    #Player 2 decide
+    p2_attack = 1
+    p2_defend = 1
+    
+    #PROCESS
+    #Players
+    p1Vp2 = p2_defend - p1_attack
+    p2Vp1 = p1_defend - p2_attack
+    p1_health -= p1Vp2
+    p2_health -= p2Vp1
+    p1_alive = p1_health > 0 
+    p2_alive = p1_health > 0 
+    #clock
+    cycle += 1
+    timer = MAX_CYCLE - cycle
+    stillTime = timer >= 0 
+    PLAY = (stillTime == p1_alive == p2_alive)
+    print(f'End Cycle Continue: {PLAY}')
+    
