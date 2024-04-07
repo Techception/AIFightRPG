bd4ddd2d28ddee478e01ef7062c20715ddd16bc7
EC2 Default User <ec2-user@ip-172-16-0-65.ec2.internal>
2024-04-07 21:33:50 +0000

    premise of development

---
+++ main.py
@@ -0,0 +1,19 @@
+#Looking tomake a fighting game 
+#an AI fighting game RPG where the characters will evolve in different ways
+#first will be a basic test game 
+#then plan to add graphical elements
+#
+#CONSTANTS:
+# events per cycle
+# directions 
+# moves
+#VARIABLES:
+# character position 
+# health 
+# timer
+#CYCLE
+#Start -> 
+#   Status -> 
+#   Player 1 decide -> Player 2 decide - > 
+#   Execute ->
+# END -> 
