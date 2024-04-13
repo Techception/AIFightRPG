c88c49ebc1f9e8f6543198cd8f82b3a020ee02cf
EC2 Default User <ec2-user@ip-172-16-0-65.ec2.internal>
2024-04-13 17:47:10 +0000

    turn based combat, the attack is based on the cycle and the characters speed stat

--- main.py
+++ main.py
@@ -1,57 +1,76 @@
+import random
+
+PLAY = True 
+
 #max values 
 MAX_CYCLE = 60
 MAX_HEALTH = 16
 MAX_ATTACK = 12
 
-#initial payer 
-p1_alive = True 
-p2_health = MAX_HEALTH
+#initial player 
+class character:
+    def __init__(self, MAX_HEALTH:int=60, speed:int=1) -> None:
+        self.health = MAX_HEALTH
+        self.KO = False
+        self.speed = speed
+        
+    def attack(self,cycle) -> bool:
+        turn = cycle % self.speed == 0
+        if turn:
+            return random.choice([True, False])
+        else: 
+            return False
+            
+    def take_damage(self, opponentAction:bool) -> None: 
+        self.health -= opponentAction
+        self.KO = self.health < 0
 
-p1_health = MAX_HEALTH
-p2_alive = True
-
-#initial clock
-timer = MAX_CYCLE
-stillTime = 1 
-
-
-p1Vp2 = 0 
-p2Vp1 = 0 
-
-
-PLAY = True 
-cycle = 0
-#Main Cylce
-while PLAY:
-    #Status
+def status():
     print("---")
-    print(f'Start Cycle Continue: {PLAY}')
-    print(f'Time: {timer}')
-    print(f'p1Vp2: {p1Vp2}')
-    print(f'p2Vp1: {p2Vp1}')
-    print(f'p1_health: {p1_health}')
-    print(f'p2_health: {p2_health}')
+    #print(f'Start Cycle Continue: {PLAY}')
+    print(f'Time: {MAX_CYCLE - cycle}')
+    print(f'p1_health: {p1.health}')
+    print(f'p2_health: {p2.health}')
+    
+def who_won(p1:object, p2:object):
+    if p1.KO and p2.KO:
+        print('draw game')
+    elif p2.KO:
+        print('Player 1 wins')
+    elif p1.KO:
+        print('player 2 wins')
+        
+        
+########################################################
+p1 = character(MAX_HEALTH)
+p2 = character(MAX_HEALTH)
+#Main Cylce
+cycle = -1
+while cycle < MAX_CYCLE: #continue if time remain 
+    cycle += 1
+    
+    #Status
+    status()
+    if p1.KO or p2.KO: #if a character is KO'd then who won? 
+        who_won(p1, p2)
+        break
     
     #PLAYER
-    #Player 1 decide
-    p1_attack = 1
-    p1_defend = 1
-    #Player 2 decide
-    p2_attack = 1
-    p2_defend = 1
-    
+    #Player 1 and 2 decide on action 
+    actionQueue = {
+        'p1': p1.attack(cycle),
+        'p2': p2.attack(cycle)
+    }
+    #print("type ", type(actionQueue['p1']))
+
     #PROCESS
     #Players
-    p1Vp2 = p2_defend - p1_attack
-    p2Vp1 = p1_defend - p2_attack
-    p1_health -= p1Vp2
-    p2_health -= p2Vp1
-    p1_alive = p1_health > 0 
-    p2_alive = p1_health > 0 
-    #clock
-    cycle += 1
-    timer = MAX_CYCLE - cycle
-    stillTime = timer >= 0 
-    PLAY = (stillTime == p1_alive == p2_alive)
-    print(f'End Cycle Continue: {PLAY}')
+    #calculate damage
+    reactionQueue = {
+        'p1': p1.take_damage(actionQueue['p2']),
+        'p2': p2.take_damage(actionQueue['p1'])
+    }
+        
+    
+        
     
