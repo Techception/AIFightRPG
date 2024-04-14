1fa7875ae6e0979787c2afad3342f3e3379e8a38
EC2 Default User <ec2-user@ip-172-16-0-65.ec2.internal>
2024-04-14 13:29:43 +0000

    improvments to the animaton cycle.  created a draw function but will need to visit this as draw needs to draw the whole screen not just the character

--- animations.py
+++ animations.py
@@ -1,28 +1,38 @@
 import os
-import time 
+import time
+import random
 
 clear = lambda: os.system('clear')
 
 neutral = "0  \n|  \n|  \n"
 
-attack_frame1 = "0  \n|v \n|  \n"
-attack_frame2 = " 0 \n<|-\n | \n"
-attack_frame3 = "0  \n|--\n|  \n"
+attack_graphic_frame1 = "0  \n|v \n|  \n"
+attack_graphic_frame2 = " 0 \n<|-\n | \n"
+attack_graphic_frame3 = " 0  \n<|--\n/ 7 \n"
 
-print(neutral)
-print(attack_frame1)
-print(attack_frame2)
-print(attack_frame3)
+attack_frame = {'hit':None,'graphic':None}
+attack_frame1 = {'hit':False,'graphic':attack_graphic_frame1}
+attack_frame2 = {'hit':False,'graphic':attack_graphic_frame2}
+attack_frame3 = {'hit':True,'graphic':attack_graphic_frame3}
 
-cycle = [neutral, attack_frame1, attack_frame2, attack_frame3, attack_frame2, attack_frame1, neutral]
+animation_cycle_attack = [attack_frame1, attack_frame2, attack_frame3, attack_frame2, attack_frame1]
 
-
-frames = cycle[:]
-while True:
+def draw(frame:dict):
     clear()
-    if len(frames) == 0:
-        frames = cycle[:]
-    else:
-        frame = frames.pop()
-        print(frame)
-        time.sleep(1)
+    print(frame['graphic'])
+    time.sleep(0.5)
+    
+
+if __name__ == "__main__":
+    #print(neutral)
+    print(attack_frame1['graphic'])
+    print(attack_frame2['graphic'])
+    print(attack_frame3['graphic'])
+    
+    frames = animation_cycle_attack[:]
+    while True:
+        if len(frames) == 0:
+            frames = animation_cycle_attack[:]
+        else:
+            frame = frames.pop()
+            draw(frame)
--- classes.py
+++ classes.py
@@ -1,26 +1,12 @@
-import random
-import copy
+import animations
 
 class character:
     def __init__(self, name:str='nemo', MAX_HEALTH:int=60, speed:int=1) -> None:
-        self.state = {'neutral': True,'attack': False,'defend': False}
-        self.cooldown = 0
-        self.startup = 0
         self.KO = False
         self.name = name
         self.health = MAX_HEALTH
-        self.speed = speed
         self.actionQueue = []
         
-    def __str__(self):
-        #currentState 
-        currentState = [k for k,v in self.state.items() if v]
-        status = f'{self.name} {currentState}: {self.health} (HP) {self.cooldown} (CD)'
-        return status
-        
-    #def decide(self):
-    #    if len(self.actionQueue) == 0:
-    #        self.attack_queue()
 
     def act(self):
         #queue the next action if queue is empty 
@@ -29,15 +15,17 @@ class character:
         #pop off the last item in the queue and action 
         if len(self.actionQueue) > 0:
             action = self.actionQueue.pop()
+            animations.draw(action)
         return action
         
     def attack_queue(self):
-        attackDamage = random.choice([True,False])
-        attackSequence = [False, False, attackDamage, False, False]
-        self.actionQueue += attackSequence
+        self.actionQueue += animations.animation_cycle_attack
     
         
 if __name__ == '__main__':
-    print(character.attackSequence)
+    p1 = character()
+    
+    while True:
+        p1.act()
 
     
